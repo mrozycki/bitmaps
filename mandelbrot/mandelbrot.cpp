7aecb34f13865562c149692205c89ad1f7051ecb
Mariusz Różycki <mar.rozycki@gmail.com>
2021-07-01 18:04:40 +0200

    Minor fixes ahead of presentation

--- mandelbrot/mandelbrot.cpp
+++ mandelbrot/mandelbrot.cpp
@@ -4,12 +4,13 @@
 int main() {
     constexpr auto scale = 200.0;
     constexpr auto width = 700, height = 400;
-    std::cout << "P1 700 400\n";
+    std::cout << "P1 " << width << " " << height << "\n";
     for (auto y = 0; y != height; ++y) {
         for (auto x = 0; x != width; ++x) {
-            std::complex<double> c0{x / scale - 2.5, y / scale - 1.0};
-            std::complex<double> z(0.0, 0.0);
+            std::complex<double> const c0{x / scale - 2.5, y / scale - 1.0};
             constexpr auto max_iteration = 255;
+
+            std::complex<double> z(0.0, 0.0);
             auto iteration = 0;
             while (std::abs(z) < 2 && iteration != max_iteration) {
                 z = z*z + c0;
